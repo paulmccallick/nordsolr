95b50b2953b580efe8b707c80b067ea6fe17ee99
Paul McCallick <paul@mccallick.com>
2016-02-15 20:58:38 -0800

    solr service now running and ec2 tools installed

--- cookbooks/nordsolr/metadata.rb
+++ cookbooks/nordsolr/metadata.rb
@@ -5,3 +5,5 @@ license 'all_rights'
 description 'Installs/Configures nordsolr'
 long_description 'Installs/Configures nordsolr'
 version '0.1.0'
+
+depends 'java'
--- cookbooks/nordsolr/recipes/default.rb
+++ cookbooks/nordsolr/recipes/default.rb
@@ -1,11 +1,24 @@
-
+solr_version = '5.4.1'
 node.default['java']['jdk_version'] = '8'
 node.default['java']['install_flavor'] = 'openjdk'
 include_recipe 'java::default'
 
+['vim','lsof','ruby', 'rsync'].each do |p|
+  package p
+end
+
+ec2_rpm_path = File.join(Chef::Config['file_cache_path'],'ec2-ami-tools.noarch.rpm')
+remote_file ec2_rpm_path  do
+  source 'http://s3.amazonaws.com/ec2-downloads/ec2-ami-tools.noarch.rpm'
+end
+
+rpm_package 'ec2-ami-tools.noarch' do
+  source ec2_rpm_path
+end
+
 solr_tar_path  =File.join(Chef::Config['file_cache_path'],'solr.tar.gz')
 remote_file solr_tar_path  do
-  source 'http://www.us.apache.org/dist/lucene/solr/5.4.1/solr-5.4.1.tgz'
+  source "http://www.us.apache.org/dist/lucene/solr/#{ solr_version }/solr-#{ solr_version }.tgz"
 end
 
 group 'solr'
@@ -13,14 +26,43 @@ user 'solr' do
   group 'solr'
 end
 
-
+solr_directory = "/opt/solr-#{ solr_version }"
 
 execute 'extract solr tar file' do
-  command "tar xzf #{ solr_tar_path } -C /opt"
-  not_if { File.exists?("/opt/solr") }
+  command <<-EOS
+  tar xzf #{ solr_tar_path } -C /opt
+  chown -R solr:solr #{ solr_directory }
+  EOS
+  not_if { File.exists?(File.join(solr_directory, "LICENSE.txt")) }
 end
 
-# link '/opt/solr' do
-#     to '/etc/file'
-#       link_type :hard
-# end
+link '/opt/solr' do
+    to solr_directory
+end
+
+file "/etc/init.d/solr" do
+  owner 'root'
+  group 'root'
+  mode 0744
+  content ::File.open(File.join(solr_directory,"bin","init.d","solr")).read
+  action :create
+end
+
+template '/etc/default/solr.in.sh' do
+  owner 'root'
+  group 'root'
+  mode 0644
+  source 'solr.in.sh'
+end
+
+['/var/solr/data','/var/log/solr'].each do |d|
+  directory d do
+    owner 'solr'
+    group 'solr'
+    recursive true
+  end
+end
+
+service 'solr' do
+  action :enable
+end
